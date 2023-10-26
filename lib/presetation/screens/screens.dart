efb73ac7564a87f1a85011c7adebfacb902dc771
JAMM0118 <jhon.alejandro.correounivalle.edu.co>
2023-10-26 13:53:08 -0500

    screens creadas y barril actualizado

--- lib/presetation/screens/screens.dart
+++ lib/presetation/screens/screens.dart
@@ -1,7 +1,12 @@
 //archivo barril
 
+export 'package:widgets_app/presetation/screens/animated/animated_screen.dart';
+export 'package:widgets_app/presetation/screens/app_tutorial/app_tutorial_screen.dart';
 export 'package:widgets_app/presetation/screens/buttons/buttons_screen.dart';
 export 'package:widgets_app/presetation/screens/cards/cards_screen.dart';
 export 'package:widgets_app/presetation/screens/home/home_screen.dart';
+export 'package:widgets_app/presetation/screens/infinite_scroll/infinite_scroll_screen.dart';
 export 'package:widgets_app/presetation/screens/nice/nice_screen.dart';
-
+export 'package:widgets_app/presetation/screens/progress/progress_screen.dart';
+export 'package:widgets_app/presetation/screens/snackbar/snackbar_screen.dart';
+export 'package:widgets_app/presetation/screens/ui_controls/ui_controls_screen.dart';
