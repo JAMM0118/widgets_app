3e3f14f5fde8625bc08f5679ed4fcd4abdc57bcc
JAMM0118 <jhon.alejandro.correounivalle.edu.co>
2024-06-15 12:21:28 -0500

    .

--- lib/main.dart
+++ lib/main.dart
@@ -27,14 +27,13 @@ class MainApp extends ConsumerWidget {
       debugShowCheckedModeBanner: false,
       theme: appTheme.getTheme(),
       
-    
-       
       /*Scaffold(
         body: Center(
           child: FilledButton(onPressed: (){}, child: Text("Hola mundo")), 
           // se da el color del tema de la app en este caso al button
         ),
       )*/
+
 
     );
   }
